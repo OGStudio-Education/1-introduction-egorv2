0e55bdbb5af7a94793d0908084a82e80095d7a4f
egorv2 <7638237+egorv2@users.noreply.github.com>
2019-02-12 23:13:06 +0500

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,6 +1,9 @@
 main.application.camera.clearColor = {1, 0, 0}
 main.application.mouse.pressedButtonsChanged:addCallback(
     function()
-        print("Detected a mouse press / release")
+        -- Detect a click.
+    if (#main.application.mouse.pressedButtons == 0)
+    then
+        print("Detected a mouse click")
     end
 )
