d79790581ffa50ca60c60b984d6cbbd6280084a6
egorv2 <7638237+egorv2@users.noreply.github.com>
2019-02-12 23:10:38 +0500

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,1 +1,6 @@
 main.application.camera.clearColor = {1, 0, 0}
+main.application.mouse.pressedButtonsChanged:addCallback(
+    function()
+        print("Detected a mouse press / release")
+    end
+)
